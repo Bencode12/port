a7160be6d81eaf5fc306b3edf62fef4f5b2012bf
Benas Petronis <benas@Benass-MacBook-Air.local>
2026-04-23 13:47:30 +0300

    Remove Interactive Mathematics component

--- build.js
+++ build.js
@@ -1,35 +1,18 @@
 const { build } = require('esbuild');
 const path = require('path');
 
-Promise.all([
-  build({
-    entryPoints: ['src/MathVisualization.jsx'],
-    bundle: true,
-    outfile: 'dist/math-viz.js',
-    minify: true,
-    sourcemap: false,
-    target: ['es2015'],
-    loader: { '.jsx': 'jsx' },
-    define: {
-      'process.env.NODE_ENV': '"production"'
-    },
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  }),
-  build({
-    entryPoints: ['src/OrbitingCircles.jsx'],
-    bundle: true,
-    outfile: 'dist/orbiting-circles.js',
-    minify: true,
-    sourcemap: false,
-    target: ['es2015'],
-    loader: { '.jsx': 'jsx' },
-    define: {
-      'process.env.NODE_ENV': '"production"'
-    },
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  })
-]).catch(() => process.exit(1));
+build({
+  entryPoints: ['src/OrbitingCircles.jsx'],
+  bundle: true,
+  outfile: 'dist/orbiting-circles.js',
+  minify: true,
+  sourcemap: false,
+  target: ['es2015'],
+  loader: { '.jsx': 'jsx' },
+  define: {
+    'process.env.NODE_ENV': '"production"'
+  },
+  alias: {
+    '@': path.resolve(__dirname, 'src')
+  }
+}).catch(() => process.exit(1));
